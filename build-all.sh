0431b220dc77c80bdcfa31f09bff8400f7462837
iBranch211 <leo.esaki211@gmail.com>
2017-02-07 14:40:52 -0600

    Updated Docker images for release 0.4.0

---
+++ build-all.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+JAVA_VERSION=8u121
+DEBEZIUM_VERSION=0.4
+MONGO_VERSION=3.2
+POSTGRES_VERSION=9.6
+
+./build-java.sh $JAVA_VERSION
+if [ $? -ne 0 ]; then
+    exit $?;
+fi
+
+./build-mongo.sh $MONGO_VERSION
+if [ $? -ne 0 ]; then
+    exit $?;
+fi
+
+./build-postgres.sh $POSTGRES_VERSION
+if [ $? -ne 0 ]; then
+    exit $?;
+fi
+
+./build-debezium.sh $DEBEZIUM_VERSION
